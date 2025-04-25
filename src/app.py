380e463ad410c48c0d0e859e2f9037639d3fdfb7
Jonathan Baugham <jonathan.baugham@jonathans-MacBook-Pro.local>
2025-04-24 19:44:05 -0600

    corrected trigger

--- src/app.py
+++ src/app.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 # ‘/’ URL is bound with hello_world() function.
 def hello_world():
     return jsonify({
-        'message': 'hello-world trigger test 2'
+        'message': 'hello-world trigger test 3'
     })
 
 @app.route('/api/v1/healthz')
