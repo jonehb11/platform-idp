7d0457e78e2c4c00536f26e34b56585584714419
Jonathan Baugham <jonathan.baugham@jonathans-MacBook-Pro.local>
2025-04-27 08:40:42 -0600

    fixed pipeline

--- src/app.py
+++ src/app.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 # ‘/’ URL is bound with hello_world() function.
 def hello_world():
     return jsonify({
-        'message': 'hello-world trigger test 12'
+        'message': 'hello-world trigger test 13'
     })
 
 @app.route('/api/v1/healthz')
