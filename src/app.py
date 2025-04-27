b3f489aa34210c4159ee1cfeb39eb3b470d0d3e3
Jonathan Baugham <jonathan.baugham@jonathans-MacBook-Pro.local>
2025-04-26 23:22:21 -0600

    testing new cd dep

--- src/app.py
+++ src/app.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 # ‘/’ URL is bound with hello_world() function.
 def hello_world():
     return jsonify({
-        'message': 'hello-world trigger test 7'
+        'message': 'hello-world trigger test 8'
     })
 
 @app.route('/api/v1/healthz')
