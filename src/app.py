f3245631a3a7a02038733f415b22e39439913fe3
Jonathan Baugham <jonathan.baugham@jonathans-MacBook-Pro.local>
2025-04-27 08:53:26 -0600

    changed path for argo login

--- src/app.py
+++ src/app.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 # ‘/’ URL is bound with hello_world() function.
 def hello_world():
     return jsonify({
-        'message': 'hello-world trigger test 13'
+        'message': 'hello-world trigger test 14'
     })
 
 @app.route('/api/v1/healthz')
